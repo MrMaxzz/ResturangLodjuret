710591e90fa0ffd3ae5e5dff7a5d67127b999fa4
21ABBMax <92145624+21ABBMax@users.noreply.github.com>
2024-03-05 10:43:35 +0100

    Added a carousel image slider

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,27 +1,45 @@
+// Code for navbar
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    // Toggle Nav
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        // Animate Links
-    navLinks.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation ='';
-        }
-        else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/10+0.1}s`;
-        }
-      });
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll(".nav-links li");
+	// Toggle Nav
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
+		// Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 10 + 0.1
+				}s`;
+			}
+		});
 
-      // Burger Animation
-      burger.classList.toggle('toggle');
-    });
-
-
-
-    
-}
+		// Burger Animation
+		burger.classList.toggle("toggle");
+	});
+};
 
 navSlide();
+
+// Code for Image Carousel
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+		const slides = button
+			.closest("[data-carousel]")
+			.querySelector("[data-slides]");
+
+		const activeSlide = slides.querySelector("[data-active]");
+		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+		if (newIndex < 0) newIndex = slides.children.length - 1;
+		if (newIndex >= slides.children.length) newIndex = 0;
+
+		slides.children[newIndex].dataset.active = true;
+		delete activeSlide.dataset.active;
+	});
+});
